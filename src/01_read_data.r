743b23f57a9d8963e5bbac1a4eb9b6a914da0afd
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-05-27 00:29:57 -0700

    Added template file to read data

---
+++ src/01_read_data.r
@@ -0,0 +1,31 @@
+#############################################################
+##                                                         ##
+## This file reads all data files required for the project ##
+##                                                         ##
+#############################################################
+
+
+
+## Source R scripts ####
+
+source(here::here("src/00c_main.r"))
+
+
+
+## Read data ####
+
+## Define the path of the data file
+data_file_path <- here::here("data/")
+
+## Checks if the file is readable and reads; if not throws an error
+if(file.access(data_file_path, mode = 4) == 0){
+  raw_data <- data.table::fread(input = data_file_path)
+} else {
+  stop("File not readable")
+}
+
+## Remove unwanted variables
+rm(data_file_path)
+
+
+
