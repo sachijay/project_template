a715e9aa61a78615cae418b27be4e5d40ade8c03
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-05-26 23:55:56 -0700

    Updated code formatting in install_packages.r

--- src/00_install_packages.r
+++ src/00_install_packages.r
@@ -6,25 +6,28 @@
 
 
 
-## List required packages from CRAN
+## Install packages from CRAN ####
 ## `tidyverse` - Packages for data manipulation and plotting
 ## `here` - Easy file referencing
 ## `data.table` - Fast data import
+
 cran_packages <- c("tidyverse",
                    "here",
                    "data.table")
 if (length(missing_pkgs <- setdiff(cran_packages, rownames(installed.packages()))) > 0) {
   message("Installing missing package(s): ", 
           paste(missing_pkgs, collapse = ", "))
-  install.packages(missing_pkgs, repos = "https://cloud.r-project.org")
+  install.packages(missing_pkgs, 
+                   repos = "https://cloud.r-project.org")
 }
 rm(cran_packages, missing_pkgs)
 
 
 
-## List required packages from Bioconductor
+## Install packages from Bioconductor ####
 ## Note: Uncomment the following lines to install packages. 
 ## Note: Install `BiocManager` from CRAN before running these lines.
+
 # bioc_packages <- c()
 # if (length(missing_pkgs <- setdiff(bioc_packages, rownames(installed.packages()))) > 0) {
 #   message("Installing missing package(s): ", 
