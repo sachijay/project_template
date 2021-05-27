c377401b200bd9dad5fab2d3c7ab5c89fcc1e015
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-05-27 00:03:32 -0700

    Added numbers source file names

--- src/00a_install_packages.r
+++ src/00a_install_packages.r
@@ -19,6 +19,8 @@ if (length(missing_pkgs <- setdiff(cran_packages, rownames(installed.packages())
           paste(missing_pkgs, collapse = ", "))
   install.packages(missing_pkgs, 
                    repos = "https://cloud.r-project.org")
+} else {
+  message("All CRAN packages already installed")
 }
 rm(cran_packages, missing_pkgs)
 
@@ -30,8 +32,10 @@ rm(cran_packages, missing_pkgs)
 
 # bioc_packages <- c()
 # if (length(missing_pkgs <- setdiff(bioc_packages, rownames(installed.packages()))) > 0) {
-#   message("Installing missing package(s): ", 
+#   message("Installing missing package(s): ",
 #           paste(missing_pkgs, collapse = ", "))
 #   BiocManager::install(missing_pkgs)
+# } else {
+#   message("All Bioconductor packages already installed")
 # }
 # rm(bioc_packages, missing_pkgs)
