4d026e8e2e4f6bf0d48f4d6cc41b3862b6cc74e4
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-05-27 00:10:29 -0700

    Added main source file template

---
+++ src/00c_main.r
@@ -0,0 +1,15 @@
+###################################################################
+##                                                               ##
+## This file stores global variables and initializes the project ##
+##                                                               ##
+###################################################################
+
+
+
+## Source R scripts ####
+
+source(here::here("src/00b_functions.r"))
+
+
+
+## 
