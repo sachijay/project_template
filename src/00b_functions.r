c1b773889364b67a6fe113e416bfa6a2808bac26
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-05-27 00:05:49 -0700

    Added template file for project level functions

---
+++ src/00b_functions.r
@@ -0,0 +1,15 @@
+##############################################################
+##                                                          ##
+## This file includes common functions used in this project ##
+##                                                          ##
+##############################################################
+
+
+
+## Install packages ####
+
+source(here::here("src/00a_install_packages.r"))
+
+
+
+## 
